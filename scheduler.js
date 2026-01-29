33c155e83a5e872a1706a6a47b833d2a5ff9324f
Aaron Socher <aesocher@yahoo.com>
2026-01-29 14:08:26 -0800

    added second cron run

--- scheduler.js
+++ scheduler.js
@@ -3,7 +3,7 @@ import cron from 'node-cron';
 import { logInfo } from './logger.js';
 
 export function scheduleDaily(jobFn) {
-  cron.schedule('0 3 * * *', () => {
+  cron.schedule('0 2,15 * * *', () => {
     logInfo('Daily cleanup at ' + new Date().toISOString());
     jobFn();
   });
