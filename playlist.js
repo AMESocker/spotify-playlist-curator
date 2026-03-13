c4891bb1b697bfda4bb9e7953c93ababe0d82fb2
Aaron Socher <aesocher@yahoo.com>
2026-03-13 11:00:24 -0700

    Add limit to car playlist with time track limit

--- playlist.js
+++ playlist.js
@@ -23,28 +23,46 @@ export async function addTracks(playlistId, uris) {
   const spotify = getSpotify();
   const chunkSize = 100;
 
+   // Always add everything to main playlist
   for (let i = 0; i < uris.length; i += chunkSize) {
     const chunk = uris.slice(i, i + chunkSize);
     await withRetry(() => spotify.addTracksToPlaylist(playlistId, chunk));
   }
 
-  // Add clean tracks to second playlist
-  // if (process.env.CLEAN_PLAYLIST_ID) {
-    const cleanUris = [];
-    for (let i = 0; i < uris.length; i += chunkSize) {
-      const chunk = uris.slice(i, i + chunkSize);
-      const ids = chunk.map(uri => uri.split(':')[2]);
-      const res = await withRetry(() => spotify.getTracks(ids));
-      res.body.tracks.forEach(track => {
-        if (track && !track.explicit) cleanUris.push(track.uri);
-      });
-    }
+  // Fetch track details for car playlist filtering
+  const cleanId    = process.env.CLEAN_PLAYLIST_ID;
+  const carAllId = process.env.CAR_PLAYLIST_ALL_ID;
+  if (!cleanId && !carAllId) return;
 
-    for (let i = 0; i < cleanUris.length; i += chunkSize) {
-      const chunk = cleanUris.slice(i, i + chunkSize);
-      await withRetry(() => spotify.addTracksToPlaylist(process.env.CLEAN_PLAYLIST_ID, chunk));
-    }
-  // }
+  const timeLimit = 7 ; //Minutes
+  const CAR_MAX_DURATION_MS = timeLimit * 60 * 1000; 
+  const carCleanUris    = [];
+  const carExplicitUris = [];
+
+  for (let i = 0; i < uris.length; i += chunkSize) {
+    const chunk = uris.slice(i, i + chunkSize);
+    const ids = chunk.map(uri => uri.split(':')[2]);
+    const res = await withRetry(() => spotify.getTracks(ids));
+    res.body.tracks.forEach(track => {
+      if (!track) return;
+      if (track.duration_ms > CAR_MAX_DURATION_MS) return;
+      if (track.explicit) {
+        carExplicitUris.push(track.uri);
+      } else {
+        carCleanUris.push(track.uri);
+      }
+    });
+  }
+
+  for (let i = 0; i < carCleanUris.length; i += chunkSize) {
+    const chunk = carCleanUris.slice(i, i + chunkSize);
+    if (cleanId) await withRetry(() => spotify.addTracksToPlaylist(cleanId, chunk));
+  }
+
+  for (let i = 0; i < carExplicitUris.length; i += chunkSize) {
+    const chunk = carExplicitUris.slice(i, i + chunkSize);
+    if (explicitId) await withRetry(() => spotify.addTracksToPlaylist(explicitId, chunk));
+  }
 }
 
 export async function removeTracks(playlistId, tracks) {
